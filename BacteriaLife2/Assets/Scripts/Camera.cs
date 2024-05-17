9df00f86969e803050eef5ad765ac092b9a802c8
alisaego <girlaliceee@yandex.ru>
2024-05-17 12:06:37 +0500

    new

--- BacteriaLife2/Assets/Scripts/Camera.cs
+++ BacteriaLife2/Assets/Scripts/Camera.cs
@@ -20,18 +20,34 @@ public class Camera : MonoBehaviour
         }
         if (Time.time % 1 == 0)
         {
-            GameObject a = Instantiate(grass_teamplate);
-            float pos_x = 0, pos_y = 0;
-            int var = Random.Range(0, 5);
-            if (var == 0)
+            for (int i = 1; i < 6; i++)
             {
-                pos_x = Random.Range(5f, 8f);
+
+                GameObject a = Instantiate(grass_teamplate);
+                float pos_x = 0, pos_y = 0;
+                int var = Random.Range(0, 4);
+                if (var == 0)
+                {
+                    pos_x = Random.Range(-20f, 20f);
+                    pos_y = Random.Range(-9f, -6f);
+                }
+                else if (var == 1)
+                {
+                    pos_x = Random.Range(-17f, -12f);
+                    pos_y = Random.Range(-10f, 10f);
+                }
+                else if (var == 2)
+                {
+                    pos_x = Random.Range(-20f, 20f);
+                    pos_y = Random.Range(6f, 9f);
+                }
+                else if (var == 3)
+                {
+                    pos_x = Random.Range(12f, 17f);
+                    pos_y = Random.Range(-10f, 10f);
+                }
+                a.transform.position = GameObject.FindWithTag("MainCamera").transform.position + new Vector3(pos_x, pos_y, 10);
             }
-            else if (var == 1)
-            {
-                pos_x = Random.Range(-8f, -5f);
-            }
-            a.transform.position = new Vector3(pos_x, pos_y, 0);
         }
     }
 }
